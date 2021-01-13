24691759439fc9457f449505f6aa6a8356d7d885
HannahStarcevich <starcevichh@gmail.com>
2021-01-12 22:00:12 -0800

    updated the constructor functions and prompts

--- app.js
+++ app.js
@@ -16,25 +16,37 @@ const employees = []
 function selectRole() {
     inquirer.prompt({
         type: "list",
-        name: "choice",
+        name: "role",
         message: "what is the employee's role?",
         choices: ["Manager", "Engineer", "Intern"]
     }).then(function ({
-        choice
+        role
     }) {
-        switch (choice) {
+        switch (role) {
             case "Manager":
+                console.log("manager")
+                createEmployee()
                 updateAsManager()
                 break;
+
             case "Engineer":
+                console.log("engineer")
+                createEmployee()
                 updateAsEngineer()
                 break;
+
             case "Intern":
+                console.log("intern")
+                createEmployee()
                 updateAsIntern()
                 break;
         }
     })
+
 }
+
+selectRole()
+
 
 // prompt to create an employee with name, email and id number info
 function createEmployee() {
@@ -52,11 +64,10 @@ function createEmployee() {
             message: "What is the employee's id number?",
         }
         // ).then(function (answers) {
-        //     const addEmployee = new Employee(answers);
-        //     employees.push(answers)
-        //     selectRole()
+
         // })
-    })
+    )
+}
 
 
 function updateAsManager() {
@@ -65,43 +76,46 @@ function updateAsManager() {
         name: "office",
         message: "What is the manager's office number?"
     })
+}
 
-    function updateAsEngineer() {
-        inquirer.prompt({
-            type: "input",
-            name: "github",
-            message: "What is the engineer's github username?"
-        })
+function updateAsEngineer() {
+    inquirer.prompt({
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github username?"
+    })
+}
 
-        function updateAsIntern() {
-            inquirer.prompt({
-                type: "input",
-                name: "school",
-                message: "What is the intern's school name?"
-            })
+function updateAsIntern() {
+    inquirer.prompt({
+        type: "input",
+        name: "school",
+        message: "What is the intern's school name?"
+    })
+}
 
 
-            // Write code to use inquirer to gather information about the development team members,
-            // and to create objects for each team member (using the correct classes as blueprints!)
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
 
-            // After the user has input all employees desired, call the `render` function (required
-            // above) and pass in an array containing all employee objects; the `render` function will
-            // generate and return a block of HTML including templated divs for each employee!
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
 
-            // After you have your html, you're now ready to create an HTML file using the HTML
-            // returned from the `render` function. Now write it to a file named `team.html` in the
-            // `output` folder. You can use the variable `outputPath` above target this location.
-            // Hint: you may need to check if the `output` folder exists and create it if it
-            // does not.
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
 
-            // HINT: each employee type (manager, engineer, or intern) has slightly different
-            // information; write your code to ask different questions via inquirer depending on
-            // employee type.
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
 
-            // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-            // and Intern classes should all extend from a class named Employee; see the directions
-            // for further information. Be sure to test out each class and verify it generates an
-            // object with the correct structure and methods. This structure will be crucial in order
-            // for the provided `render` function to work! ```
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
 
-            createEmployee()
+// createEmployee()
