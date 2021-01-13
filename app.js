bceed35cf18f22a81dcdd54f16ed6697dcdfc44c
HannahStarcevich <starcevichh@gmail.com>
2021-01-13 14:36:37 -0800

    completed web application

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
+const {
+    UV_FS_O_FILEMAP
+} = require("constants");
 const employees = []
 
 // prompt to select if the employee is a manager, engineer or intern
@@ -25,20 +28,17 @@ function selectRole() {
         switch (role) {
             case "Manager":
                 console.log("manager")
-                createEmployee()
-                updateAsManager()
+                createManager()
                 break;
 
             case "Engineer":
                 console.log("engineer")
-                createEmployee()
-                updateAsEngineer()
+                createEngineer()
                 break;
 
             case "Intern":
                 console.log("intern")
-                createEmployee()
-                updateAsIntern()
+                createIntern()
                 break;
         }
     })
@@ -48,74 +48,101 @@ function selectRole() {
 selectRole()
 
 
-// prompt to create an employee with name, email and id number info
-function createEmployee() {
-    inquirer.prompt({
-            type: "input",
-            name: "name",
-            message: "What is the employee's name?",
-        }, {
-            type: "input",
-            name: "email",
-            message: "What is the employee's email?",
-        }, {
-            type: "input",
-            name: "id",
-            message: "What is the employee's id number?",
-        }
-        // ).then(function (answers) {
-
-        // })
-    )
-}
-
-
-function updateAsManager() {
+// prompt to create an manager with name, email and id number info
+function createManager() {
     inquirer.prompt({
         type: "input",
-        name: "office",
+        name: "name",
+        message: "What is the employee's name?",
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?",
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?",
+    }, {
+        type: "input",
+        name: "officeNumber",
         message: "What is the manager's office number?"
+    }).then(function (answers) {
+        const manager = new Manager(...answers);
+        employees.push(manager)
+        addEmployee()
     })
 }
 
-function updateAsEngineer() {
-    inquirer.prompt({
+// prompt to create an engineer with name, email and id number info
+function createEngineer() {
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?",
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?",
+    }, {
         type: "input",
         name: "github",
         message: "What is the engineer's github username?"
+    }]).then(function (answers) {
+        const engineer = new Engineer(answers.name, answers.email, answers.id, answers.github);
+        employees.push(engineer)
+        addEmployee()
+
     })
 }
 
-function updateAsIntern() {
+// prompt to create an intern with name, email and id number info
+function createIntern() {
     inquirer.prompt({
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?",
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?",
+    }, {
         type: "input",
         name: "school",
         message: "What is the intern's school name?"
+    }).then(function (answers) {
+        const intern = new Intern(...answers);
+        employees.push(intern)
+        addEmployee()
+
     })
 }
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-// createEmployee()
+// user selects if they want to create another employee, if not render the html with all the employee data gathered
+function addEmployee() {
+    inquirer.prompt({
+        type: "list",
+        name: "addEmployee",
+        message: "Do you want to create another employee?",
+        choices: ["Yes", "No"]
+    }).then(function ({
+        addEmployee
+    }) {
+        if (addEmployee === "Yes") {
+            selectRole()
+        } else {
+            render(employees)
+            fs.writeFile("./myteam.html", render(employees), err => {
+                if (err) throw err
+                console.log("the file was written")
+            })
+        }
+    })
+}
